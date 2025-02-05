d3d9d2bf479154ec587d6db8bd4df652bda282b1
sahitikathula809 <sahitikathula809@gmail.com>
2025-02-05 13:03:02 -0600

    1st microservice for courses CRUD

---
+++ src/main/java/com/example/demo/ECSDeploy/EcsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.ECSDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/ECSDeploy/EcsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.ECSDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
